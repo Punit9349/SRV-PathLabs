b4f5af1d060dff8fa6399086dbbbd7d8dd4c7ad7
Punit9349 <puneetgarg9349@gmail.com>
2025-03-27 03:42:17 +0530

    commit

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = "http://localhost:5000";
-// const API_BASE_URL = "http://82.29.160.72:5000";
+// const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = "http://82.29.160.72:5000";
 export default API_BASE_URL;
